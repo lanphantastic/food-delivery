1b36c429f133ea282ce294d5de0b53f438a0cb59
Lan Phan <lanc_phan@hotmail.com>
2023-01-29 00:02:02 -0500

    yarn add @react-navigation/native

--- App.tsx
+++ App.tsx
@@ -3,8 +3,9 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="flex-1 items-center justify-center bg-slate-500">
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.sdkfhsdfksdfp!</Text>
       <StatusBar style="auto" />
     </View>
   );
