5e52fe590943f446e23f4cf80042f76d1c3b864a
Lan Phan <lanc_phan@hotmail.com>
2023-01-29 00:06:05 -0500

    yarn add @react-navigation/native-stack

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-slate-500">
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.sdkfhsdfksdfp!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View className="flex-1 items-center justify-center bg-slate-500">
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>test</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
