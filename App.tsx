6cb7af1061e46efa0a73413d61b9aa009e21050a
Lan Phan <lanc_phan@hotmail.com>
2023-01-29 01:30:47 -0500

    use react-nav's built-in navigation to render page

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
   return (
     <NavigationContainer>
-      <View className="flex-1 items-center justify-center bg-slate-500">
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>test</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>  
     </NavigationContainer>
   );
 }
