7e8b65be02fbf86e7933e5083fd0e20886d4e516
Lan Phan <lanc_phan@hotmail.com>
2023-01-30 23:56:47 -0500

    created a feature mock for testing purposes

---
+++ mock/feature-mock.ts
@@ -0,0 +1,19 @@
+import { FeatureRef } from '../models/feature'
+
+export const FeatureMock = [
+  {
+    id: 1,
+    title: 'McDonalds',
+    description: 'Paid placements near you',
+  },
+  {
+    id: 2,
+    title: 'Pizza Pizza',
+    description: "Everyone's been enjoying these juicy discounts",
+  },
+  {
+    id: 3,
+    title: 'Kinton Ramen',
+    description: 'Why not support your local restaurants tonight?',
+  },
+] as FeatureRef[]
